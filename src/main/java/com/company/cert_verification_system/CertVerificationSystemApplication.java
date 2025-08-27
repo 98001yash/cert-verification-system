075f082198f6c34c5dd1e04293742043b48b63dc
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-08-27 20:47:15 +0530

    Initial commit

---
+++ src/main/java/com/company/cert_verification_system/CertVerificationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.company.cert_verification_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertVerificationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertVerificationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/cert_verification_system/CertVerificationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.cert_verification_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertVerificationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
