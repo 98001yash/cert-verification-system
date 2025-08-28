97461be5f14fc5f4ce21804f7747263f2aac49ab
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-08-28 07:21:54 +0530

    created the certificate entity

---
+++ src/main/java/com/company/cert_verification_system/entities/Certificate.java
@@ -0,0 +1,34 @@
+package com.company.cert_verification_system.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "certificates")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Certificate {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    private Student student;
+
+    @Column(nullable = false)
+    private Course course;
+
+    @Column(nullable = false)
+    private LocalDateTime issuedAt;
+}
