403444961861b98ff6a6fe4b0690961a7dd50386
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-08-28 07:31:40 +0530

    created the student repository

---
+++ src/main/java/com/company/cert_verification_system/repositoru/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.company.cert_verification_system.repositoru;
+
+import com.company.cert_verification_system.entities.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StudentRepository extends JpaRepository<Student,Long> {
+
+    Optional<Student> findByWalletAddress(String walletAddress);
+}
